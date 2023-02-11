254caf9d656cae902b0e17ee924ebdf1cc9e5e72
Rohin <git@r0h.in>
2023-02-11 01:20:27 -0500

    Lunch Control

---
+++ reg_tools/lunch.js
@@ -0,0 +1,82 @@
+const fs = require("fs");
+const file = fs.readFileSync("./export.json")
+const data = JSON.parse(file);
+
+console.log(`🏁 Loaded ${Buffer.byteLength(file)} bytes from export.json`);
+
+let students = [];
+let teachers = [];
+
+const teachersKeys = Object.keys(data.teachers);
+
+for (let i = 0; i < teachersKeys.length; i++) {
+  // Loop through teachers
+  let teacher = data.teachers[teachersKeys[i]];
+  teachers.push({
+    name: teacher.name,
+    email: teacher.email,
+    school: teacher.school,
+    phone: teacher.phone,
+  });
+
+  if (!teacher.students) {
+    continue; // No students
+  }
+
+  const studentKeys = Object.keys(teacher.students);
+
+  for (let j = 0; j < studentKeys.length; j++) {
+    // Loop through students
+    let student = teacher.students[studentKeys[j]];
+    students.push({
+      name: student.name,
+      email: student.email,
+      school: teacher.school,
+      grade: student.grade,
+      uid: studentKeys[j],
+      plen1: student.p1 || "Not selected",
+      plen2: student.p2 || "Not selected",
+      plen3: student.p3 || "Not selected",
+    });
+  }
+}
+
+console.log(
+  `🏁 Read ${teachers.length} teachers and ${students.length} students totalling ${teachers.length + students.length} records.`
+);
+
+
+// Get lunch status for each student
+for (let i = 0; i < students.length; i++) {
+  console.log(data.lunch[students[i].uid]);
+  if (data.lunch[students[i].uid] && data.lunch[students[i].uid].name) {
+    students[i].lunch = true;
+    students[i].uccid = data.lunch[students[i].uid].uccid;
+  }
+  else {
+    students[i].lunch = false;
+    students[i].uccid = "n/a";
+  }
+}
+
+
+// Organize by school
+let schools = {};
+for (let i = 0; i < students.length; i++) {
+  let student = students[i];
+  if (!schools[student.school]) {
+    schools[student.school] = [];
+  }
+  schools[student.school].push(student);
+}
+
+// Write name, lunch status, ucc id to per-school CSV
+for (let school in schools) {
+  fs.writeFileSync("./" + school + ".csv", "name,lunch,uccid\n");
+  for (let i = 0; i < schools[school].length; i++) {
+    fs.appendFileSync(
+      "./" + school + ".csv",
+      `${schools[school][i].name},${schools[school][i].lunch},${schools[school][i].uccid}\n`
+    );
+  }
+}
